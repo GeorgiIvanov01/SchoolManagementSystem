3c5cd8ccbe69eb5e257ccd700c11a9a6f808a29d
GeorgiIvanov01 <satela8909@gmail.com>
2026-01-08 15:16:44 +0000

    changed the type of key for user ID to Guid

--- SchoolManagementSystem.Data/SchoolManagementSystemDbContext.cs
+++ SchoolManagementSystem.Data/SchoolManagementSystemDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Data.Models;
 
 namespace SchoolManagementSystem.Data
 {
-    public class SchoolManagementSystemDbContext : IdentityDbContext
+    public class SchoolManagementSystemDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public SchoolManagementSystemDbContext(DbContextOptions<SchoolManagementSystemDbContext> options)
             : base(options)
